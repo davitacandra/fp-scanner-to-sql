404c94b48bd9f6509b85a8bf264e27c165121ee6
Wardi <wardi@nusa.net.id>
2023-09-24 22:32:20 +0700

    update sementara

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Fastify from 'fastify'
 import { config } from 'dotenv'
 import fastifyMySQL from '@fastify/mysql'
-import { rawParser } from './raw.parser'
+import rawParser from './raw.parser'
 import { cdataRoute } from './cdata.route'
 import { getrequestRoute } from './getrequest.route'
 import { rootRoute } from './root.route'
@@ -14,9 +14,10 @@ const MYSQL_URL = process.env.MYSQL_URL
 
 const fastify = Fastify({ logger: true })
 
+fastify.addContentTypeParser('*', rawParser)
+
 fastify.register(fastifyMySQL, { promise: true, connectionString: MYSQL_URL })
 
-fastify.register(rawParser)
 fastify.register(cdataRoute, { prefix: '/iclock/cdata' })
 fastify.register(getrequestRoute, { prefix: '/iclock/getrequest' })
 fastify.register(rootRoute)
